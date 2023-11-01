797e47d9f4f14ef532fd0528b1f4b09faf55dd73
knotknotknot <21916@students.isb.ac.th>
2023-11-01 07:55:37 +0700

    fix jump

--- Game Jam #1/Assets/code/movement.cs
+++ Game Jam #1/Assets/code/movement.cs
@@ -11,18 +11,23 @@ public class movement : MonoBehaviour
     public float thrust = 5.0f;
     public Rigidbody rb;
     private int ground = 1;
+    private BoxCollider boxCollider;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.AddForce(thrust, 0, 0, ForceMode.Impulse);
+        boxCollider = GetComponent<BoxCollider>();
+
+
         //rb = GetComponent<Rigidbody>();
     }
-
     // Update is called once per frame
     void Update()
     {
+        //void OnTriggerEnter (Collision eruiuibrui){ ground = 1; Debug.Log("g1");}
         if (Input.GetKey("d"))
         {
+            
             rb.AddForce(new Vector3(xmul, 0, 0));
             //transform.Translate(Vector3.right*xmul);
             xvel+=xmul;
@@ -33,12 +38,16 @@ public class movement : MonoBehaviour
             //transform.Translate(Vector3.left*xmul);
             xvel-=xmul;
         }
-        if (Input.GetKeyDown("space") && ground == 1)
+        if (Input.GetKeyDown("space") && isgrounded())
         {
-            //ground = 0;
+            ground = 0;
             rb.AddForce(new Vector3(0, high, 0));
         }
-        //function OnCollisionEnter2D (col : Collision2D){ ground = 1;}
-        //transform.ranslate(Vector3.right*xvel);
+    }
+    public bool isgrounded(){
+        //bool raycastHit = Physics.BoxCast(boxCollider.bounds.center, transform.localScale, Vector3.down, Quaternion.identity, 20f);
+        Vector3 vel = rb.velocity; 
+        
+        return vel.y==0;
     }
 }
